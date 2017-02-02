60f0fd0060ecb30bbe87c373bbb9c3d2ed662a74
Dominick Triola <dominick@ignovadesign.com>
2017-02-02 10:28:31 -0800

    configure cyclic animation within react components

--- lib/components/simulation/simulation.jsx
+++ lib/components/simulation/simulation.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
+import Grid from './grid';
 
 class Simulation extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    this.canvas = this.refs.canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.grid = new Grid(100, 100, this.ctx);
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    performance.mark('start-evolve');
+    this.grid.evolve();
+    performance.mark('end-evolve');
+
+    performance.mark('start-draw');
+    this.grid.draw();
+    performance.mark('end-draw');
+
+    performance.measure("durationEvolve", "start-evolve", "end-evolve");
+    performance.measure("durationDraw", "start-draw", "end-draw");
+
+    let evolveMeasures = performance.getEntriesByName('durationEvolve');
+    let drawMeasures = performance.getEntriesByName('durationDraw');
+
+    if (evolveMeasures.length === 100) {
+      console.log('Avg Evolve: ', evolveMeasures.reduce((total, measure) => {
+        return total + measure.duration;
+      }, 0) / evolveMeasures.length);
+
+      console.log('Avg Draw: ', drawMeasures.reduce((total, measure) => {
+        return total + measure.duration;
+      }, 0) / drawMeasures.length);
+    }
+
+    requestAnimationFrame(() => this.draw(this.canvas, this.ctx, this.grid));
+  }
+
   render() {
     return (
-      <canvas id="canvas" width="400" height="400"></canvas>
+      <canvas
+          ref="canvas"
+          width="400"
+          height="400"></canvas>
     );
   }
 }
