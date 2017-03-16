dce42b185e8ec4e6bded51e4790465619398fdc9
Dominick Triola <dominick@ignovadesign.com>
2017-03-15 19:18:52 -0700

    format info modal

--- lib/components/footer/footer.jsx
+++ lib/components/footer/footer.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const info = "A cellular automaton simulation consists of a grid of cells with a finite number of states. Each cell’s state can change based on a simple set of rules. In this simulation every species (a cell with a distinct state — in this case color) can eat exactly one other species, and every species can be eaten by exactly one other species. When a cell is eaten is becomes the species (color) of the cell that ate it. In terms of cellular automata jargon: species represented the number of states, resilience represents the threshold for change, and vulnerability represents a neighborhood. Adjust the settings to find cool patterns!";
+const info = (
+  <div>
+    <p>
+      A cellular automaton simulation consists of a grid of cells with
+      a finite number of states. Each cell’s state can change based on
+      a simple set of rules. In this simulation every species (a cell
+      with a distinct state — in this case color) can eat exactly one
+      other species, and every species can be eaten by exactly one other
+      species. When a cell is eaten is becomes the species (color) of the
+      cell that ate it.
+    </p>
+    <p>
+      In terms of cellular automata jargon: species represented the
+      number of states, resilience represents the threshold for change,
+      and vulnerability represents a neighborhood.
+    </p>
+    <p>
+      Adjust the settings to find cool patterns!
+    </p>
+  </div>
+);
 
 const modalExit = (toggleModal) => (
   <div
